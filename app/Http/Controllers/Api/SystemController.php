4491e2f779e5cf238cb860103ba1d60b6cb790d3
psmever <psmever@gmail.com>
2020-08-25 23:11:53 +0900

    서부 상태 체크, 시스템 공지사항 체크 추가.

--- app/Http/Controllers/Api/SystemController.php
+++ app/Http/Controllers/Api/SystemController.php
@@ -3,11 +3,53 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\ApiRootController;
+
 use Symfony\Component\Process\Process;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Response;
+
+use App\Traits\v1\SystemTrait;
+
 class SystemController extends ApiRootController
 {
+    use SystemTrait;
+
+    /**
+     * ANCHOR 서버 상태 체크용.
+     *
+     * php artisan up && php artisan down
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function check_status(Request $request)
+    {
+        return true;
+    }
+
+    /**
+     * ANCHOR 시스템 공지 사항
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function check_notice(Request $request)
+    {
+        $task = SystemTrait::checkSystemNotice();
+
+        if($task['data']) {
+            return Response::success([
+                'notice_message' => $task['data']
+            ]);
+        } else {
+            return Response::success_no_content();
+        }
+    }
+
+    public function base_data(Request $request) {}
+
+
     /**
      * GitHub WebHook Deploy
      * ANCHOR GitHub WebHook Deploy
@@ -18,7 +60,7 @@ class SystemController extends ApiRootController
     {
         if(app()->environment('production')) {
 
-            echo ":: Production Start ::".PHP_EOL;
+            echo ":: Production Deploy Start ::".PHP_EOL;
 
             $githubPayload = $request->getContent();
             $githubHash = $request->header('X-Hub-Signature');
@@ -33,7 +75,7 @@ class SystemController extends ApiRootController
                 });
             }
 
-            echo PHP_EOL.":: Production End ::".PHP_EOL;
+            echo PHP_EOL.":: Production Deploy End ::".PHP_EOL;
 
         } else {
 
