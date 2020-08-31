7e6341227e567dc831eae6b1cb696a877b505e65
psmever <psmever@gmail.com>
2020-08-31 22:32:13 +0900

    기본 로그인 페이지.

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,23 @@ Route::get('/', function () {
     return view('landing');
 });
 
+Route::get('login', function () {
+    return redirect('front/v1/home/login');
+})->name('login');
+
+
+
 Route::group(['namespace'=> 'Front', 'prefix' => 'front', 'as' => 'front.'], function () {
 
     Route::group(['prefix' => 'test', 'as' => 'test.'], function () {
         Route::get('index', 'TestController@index')->name('index');
+    });
+
+    Route::group(['namespace' => 'v1', 'prefix' => 'v1', 'as' => 'v1.'], function () {
+        Route::group(['prefix' => 'home', 'as' => 'home.'], function () {
+            Route::get('/', 'HomeController@index')->name('index');
+            Route::get('login', 'HomeController@login')->name('login');
+        });
     });
 
 });
