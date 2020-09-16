32131648049b2600c74556d7f159ece29db4a9d6
psmever <psmever@gmail.com>
2020-09-16 14:28:10 +0900

    [UPDATE] Guitar Facade 추가.

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\ResponseMacroServiceProvider::class,
 
         App\Providers\RepositoryServiceProvider::class,
+        App\Providers\GuitarFacadesServiceProvider::class,
 
     ],
 
@@ -229,6 +230,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // Site
+        'GuitarClass'   =>  App\Supports\Facades\GuitarClass::class
 
     ],
 
