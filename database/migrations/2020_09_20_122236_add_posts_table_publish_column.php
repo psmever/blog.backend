f2cac485143ec077505a034073cb411877f94387
psmever <psmever@gmail.com>
2020-09-20 14:05:01 +0900

    [UPDATE] publish column 추가.

---
+++ database/migrations/2020_09_20_122236_add_posts_table_publish_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class AddPostsTablePublishColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->enum('publish', ['Y', 'N'])->after('markdown')->default('Y')->comment('게시 유무.');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('publish');
+        });
+    }
+}
