90c29933a640f451808e999da2fba107249f4732
psmever <psmever@gmail.com>
2021-02-23 21:38:11 +0900

    [UPDATE] weather api 날짜 조정.

--- app/Repositories/v1/SpecialtyRepository.php
+++ app/Repositories/v1/SpecialtyRepository.php
@@ -28,11 +28,21 @@ class SpecialtyRepository implements SpecialtyRepositoryInterface
     public function update() {}
     public function delete() {}
 
-    // 기상청 Excel 데이터를 기본으로 날씨 에보를 가지고 옴.
+    // 기상청 Excel 데이터를 기본으로
+    // 날짜 및 시간을 기준으로 날씨 에보 조회.
     public function getTopWeatherData($params)
     {
         return VilageFcstinfoMaster::where('active', 'Y')->orderBy('version', 'DESC')->first()->with(['weathers' => function($query) use ($params) {
                 $query->where([['area_code', $params['area_code']], ['fcstDate', ">=", $params['fcstDate']], ['fcstTime', ">=", $params['fcstTime']]]);
+                $query->limit(1)->orderBy('fcstDate', 'DESC');
+            }, 'weathers.vilage'])->first();
+    }
+
+    // 날짜를 기준으로 조회.
+    public function getTopWeatherDataSub($params)
+    {
+        return VilageFcstinfoMaster::where('active', 'Y')->orderBy('version', 'DESC')->first()->with(['weathers' => function($query) use ($params) {
+                $query->where([['area_code', $params['area_code']], ['fcstDate', ">=", $params['fcstDate']]]);
                 $query->limit(1)->orderBy('fcstDate', 'DESC');
             }, 'weathers.vilage'])->first();
     }
