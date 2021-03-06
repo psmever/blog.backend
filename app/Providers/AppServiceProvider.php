d97fd41aa504b28fce11b4cb9e9e3e0448c12eff
psmever <psmever@gmail.com>
2021-03-06 10:53:36 +0900

    [UPDATE] ide_helper.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->isLocal()) {
-            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        if ($app->environment() == 'local') {
+            $app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
     }
 
