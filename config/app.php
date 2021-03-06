5a5ee6f0b5baebf512baf8df11ce5b04bc1f88b5
psmever <psmever@gmail.com>
2021-03-06 11:15:24 +0900

    [UPDATE] ide helper remove.

--- config/app.php
+++ config/app.php
@@ -179,9 +179,6 @@ return [
 
         App\Providers\RepositoryServiceProvider::class,
         App\Providers\GuitarFacadesServiceProvider::class,
-
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-
     ],
 
     /*
