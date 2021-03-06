c172db300bb8d7f95525e77f247a84b6c3ea40fd
psmever <psmever@gmail.com>
2021-03-06 11:15:24 +0900

    [UPDATE] ide helper remove.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($app->environment() == 'local') {
-            $app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-        }
+        //
     }
 
     /**
