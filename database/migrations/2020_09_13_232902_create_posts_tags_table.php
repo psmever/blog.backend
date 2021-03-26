29a3fa98c1a2ec59f05258fa005d29591fbfe4de
psmever <psmever@gmail.com>
2021-03-26 23:15:27 +0900

    [NEW] Migrations Files.

---
+++ database/migrations/2020_09_13_232902_create_posts_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts_tags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('post_id')->nullable(false)->index()->comment('post id.');
+            $table->string('tag_id', 255)->nullable()->comment('테그 id.');
+            $table->string('tag_text', 255)->nullable()->comment('테그 내용.');
+            $table->timestamps();
+
+            $table->index(['post_id', 'tag_id']);
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts_tags');
+    }
+}
