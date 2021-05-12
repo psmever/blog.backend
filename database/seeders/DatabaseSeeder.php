df150d322f389e165fd8cc5d6735db4ae6113e87
psmever <psmever@gmail.com>
2021-05-12 14:23:46 +0900

    [ADD] User Seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call([
+            CodesSeeder::class,
+            UserSeeder::class,
+//            PostsSeeder::class,
+//            PostsTagsSeeder::class,
+        ]);
     }
 }
