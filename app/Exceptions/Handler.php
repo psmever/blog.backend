7afa033230af8ccf68a9092ea68d9b9a38f86ca6
psmever <psmever@gmail.com>
2021-06-07 01:17:50 +0900

    [UPDATE] Exceptions Handler.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -72,180 +72,185 @@ class Handler extends ExceptionHandler
 //            //
 //        });
 
-
         /**
-         * CustomException
+         * 유닛 테스트 아닐때.
          */
-        $this->renderable(function (CustomException $e) {
+        if (env('APP_ENV') != "testing") {
+            /**
+             * CustomException
+             */
+            $this->renderable(function (CustomException $e) {
 
-            $this->loggingChannel = 'CustomException';
-            $error_message = $e->getMessage() ?: __('default.server.error');
-            $loggerMessage = $this->getLoggerMessage($error_message);
+                $this->loggingChannel = 'CustomException';
+                $error_message = $e->getMessage() ?: __('default.server.error');
+                $loggerMessage = $this->getLoggerMessage($error_message);
 
-            Log::channel('CustomExceptionLog')->error($loggerMessage['file']);
-            Log::channel('slack')->debug($loggerMessage['slack']);
+                Log::channel('CustomExceptionLog')->error($loggerMessage['file']);
+                Log::channel('slack')->debug($loggerMessage['slack']);
 
-            return Response::error(400, $error_message);
-        });
+                return Response::error(400, $error_message);
+            });
 
-        /**
-         * ModelNotFoundException
-         *
-         * ModelNotFoundException 를 캐치 못하고 NotFoundHttpException 로 처리 되는 증상이 있어서
-         * 아래 render 함수 추가 해줌.
-         */
+            /**
+             * ModelNotFoundException
+             *
+             * ModelNotFoundException 를 캐치 못하고 NotFoundHttpException 로 처리 되는 증상이 있어서
+             * 아래 render 함수 추가 해줌.
+             */
 //        $this->renderable(function (ModelNotFoundException $e) {
 //            echo "ModelNotFoundException";
 //            $error_message = $e->getMessage() ?: __('default.exception.model_not_found_exception');
 //            return Response::error(404, $error_message);
 //        });
 
-        /**
-         * NotFoundHttpException
-         */
-        $this->renderable(function (NotFoundHttpException $e) {
+            /**
+             * NotFoundHttpException
+             */
+            $this->renderable(function (NotFoundHttpException $e) {
 
-            $this->loggingChannel = 'NotFoundHttpException';
-            $error_message = $e->getMessage() ?: __('default.exception.notfound');
-            $loggerMessage = $this->getLoggerMessage($error_message);
+                $this->loggingChannel = 'NotFoundHttpException';
+                $error_message = $e->getMessage() ?: __('default.exception.notfound');
+                $loggerMessage = $this->getLoggerMessage($error_message);
 
-            Log::channel('NotFoundHttpLog')->error($loggerMessage['file']);
-            Log::channel('slack')->debug($loggerMessage['slack']);
+                Log::channel('NotFoundHttpLog')->error($loggerMessage['file']);
+                Log::channel('slack')->debug($loggerMessage['slack']);
 
-            return Response::error(404, $error_message);
-        });
+                return Response::error(404, $error_message);
+            });
 
-        /**
-         * MethodNotAllowedHttpException
-         */
-        $this->renderable(function (MethodNotAllowedHttpException $e) {
+            /**
+             * MethodNotAllowedHttpException
+             */
+            $this->renderable(function (MethodNotAllowedHttpException $e) {
 
-            if ( !$e ) return false;
+                if ( !$e ) return false;
 
-            $this->loggingChannel = 'MethodNotAllowedHttpException';
-            $error_message = __('default.exception.notallowedmethod');
-            $loggerMessage = $this->getLoggerMessage($error_message);
+                $this->loggingChannel = 'MethodNotAllowedHttpException';
+                $error_message = __('default.exception.notallowedmethod');
+                $loggerMessage = $this->getLoggerMessage($error_message);
 
-            Log::channel('CustomExceptionLog')->error($loggerMessage['file']);
-            Log::channel('slack')->error($loggerMessage['slack']);
+                Log::channel('CustomExceptionLog')->error($loggerMessage['file']);
+                Log::channel('slack')->error($loggerMessage['slack']);
 
-            return Response::error(405, $error_message);
-        });
+                return Response::error(405, $error_message);
+            });
 
-        /**
-         * ClientErrorException
-         */
-        $this->renderable(function (ClientErrorException $e) {
+            /**
+             * ClientErrorException
+             */
+            $this->renderable(function (ClientErrorException $e) {
 
-            $this->loggingChannel = 'ClientErrorException';
-            $error_message = $e->getMessage() ?: __('default.exception.clienttype');
-            $loggerMessage = $this->getLoggerMessage($error_message);
+                $this->loggingChannel = 'ClientErrorException';
+                $error_message = $e->getMessage() ?: __('default.exception.clienttype');
+                $loggerMessage = $this->getLoggerMessage($error_message);
 
-            Log::channel('ClientExceptionLog')->error($loggerMessage['file']);
-            Log::channel('slack')->error($loggerMessage['slack']);
+                Log::channel('ClientExceptionLog')->error($loggerMessage['file']);
+                Log::channel('slack')->error($loggerMessage['slack']);
 
-            return Response::error(412, $error_message);
-        });
+                return Response::error(412, $error_message);
+            });
 
-        /**
-         * ServerErrorException
-         */
-        $this->renderable(function (ServerErrorException $e) {
+            /**
+             * ServerErrorException
+             */
+            $this->renderable(function (ServerErrorException $e) {
 
-            $this->loggingChannel = 'ServerErrorException';
-            $error_message = $e->getMessage() ?: __('default.exception.error_exception');
-            $loggerMessage = $this->getLoggerMessage($error_message);
+                $this->loggingChannel = 'ServerErrorException';
+                $error_message = $e->getMessage() ?: __('default.exception.error_exception');
+                $loggerMessage = $this->getLoggerMessage($error_message);
 
-            Log::channel('ServerExceptionLog')->error($loggerMessage['file']);
-            Log::channel('slack')->error($loggerMessage['slack']);
+                Log::channel('ServerExceptionLog')->error($loggerMessage['file']);
+                Log::channel('slack')->error($loggerMessage['slack']);
 
-            return Response::error(500, $error_message);
-        });
+                return Response::error(500, $error_message);
+            });
 
-        /**
-         * ForbiddenErrorException
-         */
-        $this->renderable(function (ForbiddenErrorException $e) {
+            /**
+             * ForbiddenErrorException
+             */
+            $this->renderable(function (ForbiddenErrorException $e) {
 
-            $this->loggingChannel = 'ForbiddenErrorException';
-            $error_message = ($e->getMessage()) ?: __('default.exception.forbidden_error_exception');
-            $loggerMessage = $this->getLoggerMessage($error_message);
+                $this->loggingChannel = 'ForbiddenErrorException';
+                $error_message = ($e->getMessage()) ?: __('default.exception.forbidden_error_exception');
+                $loggerMessage = $this->getLoggerMessage($error_message);
 
-            Log::channel('CustomExceptionLog')->error($loggerMessage['file']);
-            Log::channel('slack')->debug($loggerMessage['slack']);
+                Log::channel('CustomExceptionLog')->error($loggerMessage['file']);
+                Log::channel('slack')->debug($loggerMessage['slack']);
 
-            return Response::error(403, $error_message);
-        });
+                return Response::error(403, $error_message);
+            });
 
-        /**
-         * AuthenticationException
-         */
-        $this->renderable(function (AuthenticationException $e) {
+            /**
+             * AuthenticationException
+             */
+            $this->renderable(function (AuthenticationException $e) {
 
-            $this->loggingChannel = 'AuthenticationException';
-            $error_message = __('default.login.unauthorized') ?: $e;
-            $loggerMessage = $this->getLoggerMessage($error_message);
+                $this->loggingChannel = 'AuthenticationException';
+                $error_message = __('default.login.unauthorized') ?: $e;
+                $loggerMessage = $this->getLoggerMessage($error_message);
 
-            Log::channel('authenticationlog')->error($loggerMessage['file']);
-            Log::channel('slack')->debug($loggerMessage['slack']);
+                Log::channel('authenticationlog')->error($loggerMessage['file']);
+                Log::channel('slack')->debug($loggerMessage['slack']);
 
-            return Response::error(401, $error_message);
-        });
+                return Response::error(401, $error_message);
+            });
 
-        /**
-         * ThrottleRequestsException
-         */
-        $this->renderable(function (ThrottleRequestsException $e) {
+            /**
+             * ThrottleRequestsException
+             */
+            $this->renderable(function (ThrottleRequestsException $e) {
 
-            $this->loggingChannel = 'ThrottleRequestsException';
-            $error_message = ($e->getMessage()) ?: __('default.exception.throttle_exception');
-            $loggerMessage = $this->getLoggerMessage($error_message);
+                $this->loggingChannel = 'ThrottleRequestsException';
+                $error_message = ($e->getMessage()) ?: __('default.exception.throttle_exception');
+                $loggerMessage = $this->getLoggerMessage($error_message);
 
-            Log::channel('CustomExceptionLog')->error($loggerMessage['file']);
-            Log::channel('slack')->debug($loggerMessage['slack']);
+                Log::channel('CustomExceptionLog')->error($loggerMessage['file']);
+                Log::channel('slack')->debug($loggerMessage['slack']);
 
-            return Response::error(429, $error_message);
-        });
+                return Response::error(429, $error_message);
+            });
 
-        /**
-         * PDOException
-         */
-        $this->renderable(function (PDOException $e) {
+            /**
+             * PDOException
+             */
+            $this->renderable(function (PDOException $e) {
 
-            $this->loggingChannel = 'PDOException';
-            $error_message = ($e->getMessage()) ?: __('default.exception.pdo_exception');
-            $loggerMessage = $this->getLoggerMessage($error_message);
+                $this->loggingChannel = 'PDOException';
+                $error_message = ($e->getMessage()) ?: __('default.exception.pdo_exception');
+                $loggerMessage = $this->getLoggerMessage($error_message);
 
-            Log::channel('PDOExceptionLog')->error($loggerMessage['file']);
-            Log::channel('slack')->debug($loggerMessage['slack']);
+                Log::channel('PDOExceptionLog')->error($loggerMessage['file']);
+                Log::channel('slack')->debug($loggerMessage['slack']);
 
-            return Response::error(
-                500,
-                __('default.exception.pdo_exception'),
-            );
-        });
+                return Response::error(
+                    500,
+                    __('default.exception.pdo_exception'),
+                );
+            });
 
-        /**
-         * EtcException
-         */
-        $this->renderable(function (Throwable $e) {
+            /**
+             * EtcException
+             */
+            $this->renderable(function (Throwable $e) {
 
-            $this->loggingChannel = 'Throwable';
-            $error_message = 'error_message : ' . __('default.exception.error_exception');
-            $error_message .= PHP_EOL.'error : ' .$e->getMessage();
-            $loggerMessage = $this->getLoggerMessage($error_message);
+                $this->loggingChannel = 'Throwable';
+                $error_message = 'error_message : ' . __('default.exception.error_exception');
+                $error_message .= PHP_EOL.'error : ' .$e->getMessage();
+                $loggerMessage = $this->getLoggerMessage($error_message);
 
-            Log::channel('CustomExceptionLog')->error($loggerMessage['file']);
-            Log::channel('slack')->debug($loggerMessage['slack']);
+                Log::channel('CustomExceptionLog')->error($loggerMessage['file']);
+                Log::channel('slack')->debug($loggerMessage['slack']);
 
-            return Response::error(
-                503,
-                [
-                    'error_message' => __('default.exception.error_exception'),
-                    'error' => $e->getMessage()
-                ]
-            );
-        });
+                return Response::error(
+                    503,
+                    [
+                        'error_message' => __('default.exception.error_exception'),
+                        'error' => $e->getMessage()
+                    ]
+                );
+            });
+        }
+
 
     }
 
