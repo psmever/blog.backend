7a62707f3e49cf8762c5908ee31426e9bae243cf
psmever <psmever@gmail.com>
2021-06-07 00:29:14 +0900

    [ADD] cron get weather.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         'App\Console\Commands\Dev\DevWorks',
+        'App\Console\Commands\Cron\CronWeather',
     ];
 
     /**
@@ -25,6 +26,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('cron:weather get')->hourly();
+        $schedule->command('cron:covid get')->cron('1 08-23 * * *');
     }
 
     /**
