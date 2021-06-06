26647cbbff89dc0e4e054e71b8a219f1067fd3ec
psmever <psmever@gmail.com>
2021-06-06 22:25:39 +0900

    [UPDATE] 코드정리.

--- app/Http/Middleware/ApiAfterMiddleware.php
+++ app/Http/Middleware/ApiAfterMiddleware.php
@@ -4,15 +4,14 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class ApiAfterMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
@@ -26,6 +25,6 @@ class ApiAfterMiddleware
      */
     public function terminate($request, $response) : void
     {
-        Log::debug('ApiAfterMiddleware terminate');
+        // 종료시?
     }
 }
