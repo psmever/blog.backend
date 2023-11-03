cb474518a8c257aed9921173bb2f1acabb1bf01a
psmever <psmever@gmail.com>
2023-11-03 17:13:35 +0900

    build: base 작업중

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['prefix' => 'test', 'as' => 'test.'], function () {
+	Route::controller(TestController::class)->group(function () {
+		Route::get('/default', 'default')->name('default');
+	});
 });
