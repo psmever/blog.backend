40a01060567172f8ed770f5749c9102294741eb6
psmever <psmever@gmail.com>
2023-11-07 11:52:41 +0900

    build: docker start script

--- docker/files/start.sh
+++ docker/files/start.sh
@@ -2,10 +2,10 @@
 
 service php8.2-fpm start
 
-tail -F /var/www/storage/logs/laravel.log &
-tail -F /var/log/php8.2-fpm.log &
-tail -F /var/log/nginx/access.log &
-tail -F /var/log/nginx/error.log &
+tail -F /var/www/storage/logs/laravel.log | xargs -I {} echo "laravel.log -> {}" &
+tail -F /var/log/php8.2-fpm.log | xargs -I {} echo "php8.2-fpm.log -> {}" &
+tail -F /var/log/nginx/access.log | xargs -I {} echo "access.log -> {}" &
+tail -F /var/log/nginx/error.log | xargs -I {} echo "error.log -> {}" &
 
 nginx -g "daemon off;"
 
