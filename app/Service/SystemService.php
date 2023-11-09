951d07f358fe4ecdf1e3ec632dee5f876de9f33d
psmever <psmever@gmail.com>
2023-11-09 17:21:53 +0900

    build: 시스템 공지 사항 추가

---
+++ app/Service/SystemService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Service;
+
+use Illuminate\Support\Facades\Storage;
+
+class SystemService
+{
+	/**
+	 *
+	 */
+	function __construct()
+	{
+		//
+	}
+
+	/**
+	 * 시스템 공지 사항 체크
+	 * @return string
+	 */
+	function systemNotice(): string
+	{
+		$noticeFileName = 'system-notice.txt';
+		$noticeExists = Storage::disk('system')->exists($noticeFileName);
+
+		// 시스템 공지 사항 없을때.
+		if (!$noticeExists) {
+			return "";
+		}
+
+		// 시스템 공지 사항 있을때.
+		$noticeContents = Storage::disk('system')->get($noticeFileName);
+		if (empty($noticeContents)) {
+			return "";
+		}
+
+		return $noticeContents;
+	}
+}
