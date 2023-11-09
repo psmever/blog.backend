9c44d4d8ad59b6a3da9549314d86592ad7cb031e
psmever <psmever@gmail.com>
2023-11-09 17:40:49 +0900

    build: ide-helper 추가

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,29 @@
 
 namespace App\Providers;
 
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+		/**
+		 * custom
+		 */
+		if ($this->app->isLocal()) {
+			$this->app->register(IdeHelperServiceProvider::class);
+		}
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
+		//
+	}
 }
