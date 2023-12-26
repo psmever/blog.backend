0dae79d747edeb0c29e0e5818c085af2e693a000
psmever <psmever@gmail.com>
2023-12-26 23:16:40 +0900

    build: ide-helper update

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,29 +20,37 @@ use Laravel\Sanctum\PersonalAccessToken;
  * App\Models\User
  *
  * @property int $id
+ * @property string $uuid 사용자 uuid
+ * @property string $type 사용자 타입
+ * @property string $level 사용자 레벨
  * @property string $name
  * @property string $email
  * @property Carbon|null $email_verified_at
  * @property mixed $password
  * @property string|null $remember_token
+ * @property string $active 사용자 상태
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  * @property-read DatabaseNotificationCollection<int, DatabaseNotification> $notifications
  * @property-read int|null $notifications_count
  * @property-read Collection<int, PersonalAccessToken> $tokens
  * @property-read int|null $tokens_count
- * @method static UserFactory factory($count = null, $state = [])
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
  * @method static Builder|User newModelQuery()
  * @method static Builder|User newQuery()
  * @method static Builder|User query()
+ * @method static Builder|User whereActive($value)
  * @method static Builder|User whereCreatedAt($value)
  * @method static Builder|User whereEmail($value)
  * @method static Builder|User whereEmailVerifiedAt($value)
  * @method static Builder|User whereId($value)
+ * @method static Builder|User whereLevel($value)
  * @method static Builder|User whereName($value)
  * @method static Builder|User wherePassword($value)
  * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereType($value)
  * @method static Builder|User whereUpdatedAt($value)
+ * @method static Builder|User whereUuid($value)
  * @mixin Eloquent
  */
 class User extends Authenticatable
