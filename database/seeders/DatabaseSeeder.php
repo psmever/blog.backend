22ef535b5d9283fb1cc8cd7f5700a174779d6df1
psmever <psmever@gmail.com>
2025-10-11 21:01:23 +0900

    기본 공통 코드 시더 추가

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            CommonCodeSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
